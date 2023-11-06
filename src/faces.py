dd50b84a2fdf8d4089280e5ac74ae56d284809be
Ben Butler-Cole <ben@bridesmere.com>
2023-11-06 20:08:46 +0000

    Use qualified imports for dependencies
    This is an experiment. Werkzeug in particular is a bit verbose because
    most things need pulling out of submodules. It might be more reasonable
    to use bare name imports later when things are in separate modules.

--- src/faces.py
+++ src/faces.py
@@ -1,19 +1,18 @@
 import os
 from dataclasses import dataclass
 
-from jinja2 import Environment, FileSystemLoader
+import jinja2
 import sqlalchemy
 import sqlalchemy.exc
-from werkzeug.exceptions import HTTPException
-from werkzeug.middleware.shared_data import SharedDataMiddleware
-from werkzeug.routing import Map, Rule
-from werkzeug.utils import redirect
-from werkzeug.wrappers import Request, Response
+import werkzeug
+import werkzeug.exceptions
+import werkzeug.middleware.shared_data
+import werkzeug.routing
+import werkzeug.wrappers
 
 class Project:
     def __init__(self, name):
         self.name = name
-
 
 class Faces:
     def __init__(self):
@@ -25,7 +24,6 @@ class Faces:
     def create_project(self, name):
         p = Project(name)
         self._database.save_project(p)
-
 
 meta_data = sqlalchemy.MetaData()
 
@@ -59,20 +57,23 @@ class Database:
 class Templates:
     def __init__(self):
         template_path = os.path.join(os.path.dirname(__file__), 'templates')
-        self._environment = Environment(loader=FileSystemLoader(template_path), autoescape=True)
+        self._environment = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(template_path),
+            autoescape=True
+        )
 
     def render(self, template_name, **context):
         t = self._environment.get_template(f'{template_name}.jinja')
-        return Response(t.render(context), mimetype='text/html')
+        return werkzeug.Response(t.render(context), mimetype='text/html')
 
 class Web:
     def __init__(self):
         self._faces = Faces()
         self._templates = Templates()
 
-        self.url_map = Map([
-            Rule('/', endpoint='index'),
-            Rule('/project', endpoint='create_project', methods=['PUT']),
+        self.url_map = werkzeug.routing.Map([
+            werkzeug.routing.Rule('/', endpoint='index'),
+            werkzeug.routing.Rule('/project', endpoint='create_project', methods=['PUT']),
         ])
 
     def on_index(self, _request, _urls):
@@ -82,7 +83,7 @@ class Web:
     def on_create_project(self, request, urls):
         name = request.form['name']
         self._faces.create_project(name)
-        return redirect(urls.build('index'))
+        return werkzeug.utils.redirect(urls.build('index'))
 
     def dispatch(self, request):
         urls = self.url_map.bind_to_environ(request)
@@ -94,24 +95,22 @@ class WSGIApp:
         self._web = Web()
 
     def __call__(self, environ, start_response):
-        request = Request(environ)
+        request = werkzeug.Request(environ)
         try:
             response = self._web.dispatch(request)
-        except HTTPException as e:
+        except werkzeug.exceptions.HTTPException as e:
             return e
         return response(environ, start_response)
 
-
 def create_app():
     app = WSGIApp()
-    return SharedDataMiddleware(
+    return werkzeug.middleware.shared_data.SharedDataMiddleware(
         app, {'/static': os.path.join(os.path.dirname(__file__), 'static')}
     )
 
-
 if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-
-    run_simple('127.0.0.1', 5000,
-               create_app(),
-               use_debugger=True, use_reloader=True)
+    werkzeug.run_simple(
+        '127.0.0.1', 5000,
+        create_app(),
+        use_debugger=True, use_reloader=True
+    )
