bb8a4c20977fc693c53540ae5613f408b5193f75
Ben Butler-Cole <ben@bridesmere.com>
2023-11-06 21:40:57 +0000

    Store database connection in context var

--- src/faces.py
+++ src/faces.py
@@ -1,9 +1,11 @@
 import os
+from contextvars import ContextVar
 from dataclasses import dataclass
 
 import jinja2
 import sqlalchemy
 import sqlalchemy.exc
+import sqlalchemy.schema
 import werkzeug
 import werkzeug.exceptions
 import werkzeug.middleware.shared_data
@@ -25,34 +27,49 @@ class Faces:
         p = Project(name)
         self._database.save_project(p)
 
-meta_data = sqlalchemy.MetaData()
-
 @dataclass
 class Tables:
-    projects = sqlalchemy.Table('projects', meta_data,
+    projects = sqlalchemy.Table('projects', sqlalchemy.MetaData(),
                                 sqlalchemy.Column('name', sqlalchemy.Text))
 tables = Tables()
 
 class Database:
     def __init__(self):
         self._engine = sqlalchemy.create_engine('sqlite+pysqlite:///faces.db', echo=True)
-        with self._engine.connect() as c:
-            try:
-                c.execute(sqlalchemy.select(tables.projects.c.name))
-            except sqlalchemy.exc.OperationalError:
-                meta_data.create_all(self._engine)
-                c.execute(sqlalchemy.insert(tables.projects), [{'name': 'foo'}, {'name': 'bar'}])
-                c.commit()
+        self._context_var = ContextVar('connection')
+        self.initialize()
+
+    def initialize(self):
+        try:
+            self.execute(sqlalchemy.select(tables.projects.c.name))
+        except sqlalchemy.exc.OperationalError:
+            self.execute(sqlalchemy.schema.CreateTable(tables.projects))
+            self.execute(sqlalchemy.insert(tables.projects),
+                         [{'name': 'foo'}, {'name': 'bar'}])
+            self.commit()
 
     def all_projects(self):
-        result = self._engine.connect().execute(sqlalchemy.select(tables.projects.c.name))
+        result = self.execute(sqlalchemy.select(tables.projects.c.name))
         projects = [Project(row.name) for row in result]
         return projects
 
     def save_project(self, project):
-        c = self._engine.connect()
-        c.execute(sqlalchemy.insert(tables.projects).values(name=project.name))
-        c.commit()
+        s = sqlalchemy.insert(tables.projects).values(name=project.name)
+        self.execute(s)
+        self.commit()
+
+    def execute(self, statement, parameters=None):
+        return self._connection().execute(statement, parameters)
+
+    def commit(self):
+        self._connection().commit()
+
+    def _connection(self):
+        c = self._context_var.get(None)
+        if not c:
+            c = self._engine.connect()
+            self._context_var.set(c)
+        return c
 
 class Templates:
     def __init__(self):
